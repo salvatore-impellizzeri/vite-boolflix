26336ab15014f826ead0cd316292374866d77c85
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-09-16 16:09:50 +0200

    Flag language

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -4,7 +4,13 @@ import { store } from '../store.js';
 export default {
   data() {
     return {
-
+        store,
+        language: {
+                EN: "https://flagsapi.com/GB/flat/64.png",
+                JA: "https://flagsapi.com/JP/flat/64.png",
+                IT: "https://flagsapi.com/IT/flat/64.png",
+                FR: "https://flagsapi.com/FR/flat/64.png",
+        }
     }
   },
 
@@ -13,7 +19,10 @@ export default {
    },
 
   methods: {
-
+    getFlagLanguage(){
+        const languageCode = this.movie.original_language.toUpperCase();
+        return this.language[languageCode] || "";
+    }
   }
 }
 </script>
@@ -21,10 +30,13 @@ export default {
 <template> 
     <div class="w-100 bg-dark border border-secondary my-container-card">
         <div>
-            <img :src="`https://image.tmdb.org/t/p/original${movie.poster_path}`" alt="movie.title">
+            <!-- <img :src="`https://image.tmdb.org/t/p/original${movie.poster_path}`" alt="movie.title"> -->
         </div>
-        <div>
+        <div class="text-white">
             <h3>{{ movie.title }}</h3>
+            <h3>{{ movie.original_title }}</h3>
+            <img :src="getFlagLanguage()" alt="" v-if="getFlagLanguage()">
+            <h3>{{ movie.vote_average }}</h3>
         </div>
     </div>
 </template>
