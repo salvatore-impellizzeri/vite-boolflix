231f1e6e6859473f1ec74fdcfc32918481c403e8
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-09-18 17:59:56 +0200

    Modificato header estetica

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,8 +6,41 @@ export default {
   data() {
     return {
       store,
+      links: [
+        {
+          label:"Home",
+          href:"#",
+        },
+        {
+          label:"Serie TV",
+          href:"#",
+        },
+        {
+          label:"Film",
+          href:"#",
+        },
+        {
+          label:"Originali",
+          href:"#",
+        },
+        {
+          label:"Aggiunti di recente",
+          href:"#",
+        },
+        {
+          label:"La mia lista",
+          href:"#",
+        },
+      ],
+      active: 0,
     }
   },
+
+  methods: {
+    toggleLink(index){
+      this.active = index;
+    }
+  }
 }
 </script>
 
@@ -20,28 +53,15 @@ export default {
       <div class="col-4 d-flex justify-content-end">
         <form @submit.prevent="search" class="w-100">
           <input type="search" v-model="store.SearchInput" class="me-3 w-75" placeholder="Cerca un film...">
-          <button @click="search" type="submit">Cerca</button>
+          <button @click="search" class="btn btn-light fw-medium" type="submit">Cerca</button>
         </form>
       </div>
       <div class="col-4 d-flex justify-content-end">
-        <ul class="nav justify-content-end">
-          <li class="nav-item">
-            <a class="nav-link fw-bold active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-mediumm" href="#">Serie TV</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-mediumm" href="#">Film</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-mediumm" href="#">Originali</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-mediumm" href="#">Aggiunti di recente</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-mediumm" href="#">La mia lista</a>
+        <ul class="nav gap-4 justify-content-end">
+          <li class="nav-item" v-for="(link, index) in links" key="index">
+            <a :href="link.href" @click="toggleLink(index)" :class="{ 'active-text' : active === index }">
+              {{ link.label }}
+            </a>
           </li>
         </ul>
       </div>
@@ -56,17 +76,14 @@ export default {
 
 input{
   background-color: transparent;
-  border-radius: 20px;
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.466);
   padding: 5px 10px;
   color: white;
 }
 
 button{
   border-radius: 20px !important;
-  border: 1px solid rgb(255, 255, 255);
-  background-color: transparent;
-  color: white;
-  padding: 5px 15px;
 }
 
 h1 {
@@ -74,6 +91,20 @@ h1 {
 }
 
 .nav-link{
-  color: white !important;
+  color: rgba(255, 255, 255, 0.637) !important;
 }
+
+.active-text{
+  font-weight: bold;
+  color: white;
+}
+
+li > a{
+  text-decoration: none;
+  color: rgba(223, 223, 223, 0.7);
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
 </style>
